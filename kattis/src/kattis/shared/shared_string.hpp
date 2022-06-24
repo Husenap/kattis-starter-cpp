dd6580e493a25d754969f82f8c4733e20cf3338f
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2022-06-24 13:54:27 +0200

    added string formatter

--- kattis/src/kattis/shared/shared_string.hpp
+++ kattis/src/kattis/shared/shared_string.hpp
@@ -16,4 +16,20 @@ inline std::vector<std::string> split(const std::string& s, char delimiter) {
   return tokens;
 }
 
+template <typename... Args>
+inline void format_to(std::ostream& out, std::string_view format_string, Args&&... args) {
+  ((out << format_string.substr(0, format_string.find("{}")),
+    format_string.remove_prefix(format_string.find("{}") + 2),
+    out << args),
+   ...);
+  out << format_string;
+}
+
+template <typename... Args>
+inline auto format(std::string_view format_string, Args&&... args) {
+  std::ostringstream output;
+  format_to(output, format_string, std::forward<Args>(args)...);
+  return output.str();
+}
+
 }  // namespace shared
